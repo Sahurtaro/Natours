a77c018e534a3dec44533f4ffbc30c5b16f9af1e
santiago hurtado <sahurtaro@gmail.com>
2022-10-31 16:23:25 -0500

    55. Handling patch request

---
+++ app.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+app.use(express.json()); //Middleware para que express pueda leer lo que viene por req.body
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side', app: 'Natours' }); //status 200 para ok
+// }); //definimos la ruta básica
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`) //se lee los datos que están en el archivo de texto y se transforman a formato JSON
+);
+app.get('/api/v1/tours', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', results: tours.length, data: { tours: tours } });
+}); //La función callback se llama Route Handler
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1; //transformamos el id que viene por params(que es un string), en un número
+  const tour = tours.find((el) => el.id === id);
+  //   if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({ status: 'Failed', message: 'Invalid ID' });
+  }
+
+  res.status(200).json({ status: 'success', data: { tour } });
+}); //La función callback se llama Route Handler
+
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1; //aquí se crea un índica mayor al índice del último objeto en el archivo
+  const newTour = Object.assign({ id: newId }, req.body); //con esto creamos un nuevo tour combinando el nuevo id creado con la info que viene por el body
+  tours.push(newTour); //agregamos este nuevo objeto al arreglo de tours
+  fs.writeFile(
+    //aquí agregamos el nuevo tour al archivo de texto donde tenemos todos los tours y funciona provicionalmente como base de datos
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tour: newTour } }); //Status 201 indica Created
+    }
+  );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'Failed', message: 'Invalid ID' });
+  }
+  res
+    .status(200)
+    .json({ status: 'success', data: { tour: '<Updated tour here...>' } });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Running on port ${port}...`);
+}); //Así se inicializa el servidor, recibe como parámetros el puerto y un callback

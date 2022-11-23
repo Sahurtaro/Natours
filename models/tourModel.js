00a512fa219424c13cba742a4d5fad499fcc785a
santiago hurtado <sahurtaro@gmail.com>
2022-11-22 22:27:22 -0500

    104. Virtual properties

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,52 +1,66 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'], //valor requerido y error
-    unique: true,
-    trim: true, //solo funciona para strings y remueve todos los espacios en blanco al inicio y al final del string
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'], //valor requerido y error
+      unique: true,
+      trim: true, //solo funciona para strings y remueve todos los espacios en blanco al inicio y al final del string
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: { type: Number, default: 4.5 },
+    ratingsQuantity: { type: Number, default: 0 },
+    price: {
+      type: Number,
+      required: [true, 'The tour must have a price'], //valor requerido y error
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true, //solo funciona para strings y remueve todos los espacios en blanco al inicio y al final del string
+      required: [true, 'A tour must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String], //acá aclaro que para esta propiedad quiero un arreglo de strings
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date], //acá aclaro que para esta propiedad quiero un arreglo de fechas
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  ratingsAverage: { type: Number, default: 4.5 },
-  ratingsQuantity: { type: Number, default: 0 },
-  price: {
-    type: Number,
-    required: [true, 'The tour must have a price'], //valor requerido y error
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true, //solo funciona para strings y remueve todos los espacios en blanco al inicio y al final del string
-    required: [true, 'A tour must have a description'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String], //acá aclaro que para esta propiedad quiero un arreglo de strings
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-  startDates: [Date], //acá aclaro que para esta propiedad quiero un arreglo de fechas
+  {
+    toJSON: { virtuals: true }, //cada vez que los datos se muestren como JSON queremos que virtuals sean parte del resultado
+    toObject: { virtuals: true }, //cada vez que los datos se muestren como Objects queremos que virtuals ean parte del resultado
+  }
+); //el segundo objeto que pasamos como argumento al Schema es de opciones
+
+//Virtual features are fields that we can define in our schema but will not be persisted or not be saved
+
+tourSchema.virtual('durationWeeks').get(function () {
+  //los virtuals no se pueden usar en query, porque no son parte de la base de datos realmente
+  return this.duration / 7;
 });
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;

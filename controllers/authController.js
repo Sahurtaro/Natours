dd47c2eda72755a0705a2533f79def317b39cf8b
santiago hurtado <sahurtaro@gmail.com>
2022-12-04 19:26:34 -0500

    131. Protecting tour routes. Part 1

--- controllers/authController.js
+++ controllers/authController.js
@@ -40,3 +40,27 @@ exports.login = catchAsync(async (req, res, next) => {
   const token = signToken(user._id);
   res.status(200).json({ status: 'success', token });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  //1) Getting token and check if it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  console.log(token);
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access', 401) //401 means unauthorized
+    );
+  }
+  //2) Token verification
+
+  //3) Check if user still exists
+
+  //4) Check if user changed password after the token was issued
+
+  next();
+});

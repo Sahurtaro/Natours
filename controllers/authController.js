77f9c68f8ebdfb4d36a5895a7e7d5464fe81abf8
santiago hurtado <sahurtaro@gmail.com>
2022-12-05 15:49:41 -0500

    mod

--- controllers/authController.js
+++ controllers/authController.js
@@ -61,14 +61,14 @@ exports.protect = catchAsync(async (req, res, next) => {
   const decoded = await promosify(jwt.verify)(token, process.env.JWT_SECRET);
 
   //3) Check if user still exists
-  const freshUser = await User.findById(decoded.id); //verificamos que el usuario aún exista después de generado el token
-  if (!freshUser) {
+  const currentUser = await User.findById(decoded.id); //verificamos que el usuario aún exista después de generado el token
+  if (!currentUser) {
     return next(
       AppError('The user belonging to this token does no longer exists', 401)
     );
   }
   //4) Check if user changed password after the token was issued
-  if (freshUser.changedPasswordAfter(decoded.iat)) {
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
     return next(
       new AppError('User recently changed password! Please login again', 401)
     );
@@ -76,6 +76,6 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   // GRANT ACCESS TO PROTECTED ROUTE
 
-  req.user = freshUser;
+  req.user = currentUser;
   next();
 });

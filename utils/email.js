69108a186516fd8544be15e6d9d9ac90a4ad5ae9
santiago hurtado <sahurtaro@gmail.com>
2023-01-23 12:12:22 -0500

    208. Sending password reset emails.

--- utils/email.js
+++ utils/email.js
@@ -47,4 +47,10 @@ module.exports = class Email {
   async sendWelcome() {
     await this.send(`Welcome`, 'Welcome to the Natours family!');
   }
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)'
+    );
+  }
 };

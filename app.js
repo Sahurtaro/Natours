0be21b99d1783954bcb6a5730dec4678459c124f
santiago hurtado <sahurtaro@gmail.com>
2023-03-06 16:03:05 -0500

    180B. Extending our base template with blocks

--- app.js
+++ app.js
@@ -78,7 +78,20 @@ app.use((req, res, next) => {
 
 // 3)ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'jonas',
+  });
+});
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
 });
 app.use('/api/v1/tours', tourRouter); //middleware
 app.use('/api/v1/users', userRouter); //middleware

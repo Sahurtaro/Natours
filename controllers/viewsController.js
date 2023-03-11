852c6e348a7120fed75dcad36165319102406d1c
santiago hurtado <sahurtaro@gmail.com>
2023-03-10 19:10:07 -0500

    182B. Building the tour overwiew. part 1.1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,15 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //1) Get tour data from collection
+  const tours = await Tour.find();
+  //2) Build template
+  //3) Render that template using the tour data from 1)
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',

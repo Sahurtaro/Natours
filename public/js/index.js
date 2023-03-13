d7980338a7ba289c69a863691616e2e941f2c8fa
santiago hurtado <sahurtaro@gmail.com>
2023-03-13 11:44:16 -0500

    191B. Loggin in users with our api. part 3

---
+++ public/js/index.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+import '@babel/polyfill';
+import { login } from './login';
+import { displayMap } from './mapbox';
+
+// DOM ELEMENTS
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form');
+const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
+
+// DELEGATION
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
+}
+
+if (loginForm)
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,8 @@
-// import { showAlert } from './alerts';
-// import axios from 'axios';
+/* eslint-disable */
+import { showAlert } from './alerts';
+import axios from 'axios';
 
-const login = async (email, password) => {
+export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
@@ -10,19 +11,12 @@ const login = async (email, password) => {
     });
     console.log(res);
     if (res.data.status === 'success') {
-      alert('Logged in successfully!');
+      showAlert('success', 'Logged in successfully!');
       window.setTimeout(() => {
         location.assign('/');
       }, 1500);
     }
   } catch (err) {
-    alert(err.response.data.message);
+    showAlert('error', err.response.data.message);
   }
 };
-
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});

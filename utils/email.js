2b24eab678c2fa645828ecb06442de7f0d14787d
santiago hurtado <sahurtaro@gmail.com>
2023-03-20 12:11:52 -0500

    207B. Email templates with pug: welcome emails

--- utils/email.js
+++ utils/email.js
@@ -27,10 +27,11 @@ module.exports = class Email {
   //Send the actual email
   async send(template, subject) {
     //1) Render HTML based on a pug template
-    const html = pug.renderFile(
-      `${__dirname}/../views/emails/${template}.pug`,
-      { firstName: this.firstName, url: this.url, subject }
-    );
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
     //2) Define the email options
     const mailOptions = {
       from: this.from,
@@ -41,8 +42,9 @@ module.exports = class Email {
       // html:
     };
     //3) Create a transport and send email
-    this.newTransport();
-    await transporter.sendMail(mailOptions); //esto devuelve una promesa, así que toca usar async-await
+    // this.newTransport();
+    // await transporter.sendMail(mailOptions); //esto devuelve una promesa, así que toca usar async-await
+    await this.newTransport().sendMail(mailOptions);
   }
 
   async sendWelcome() {
